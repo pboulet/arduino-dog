a59f091c79538a6753d336316b3265452c5c8402
patriceboulet <pboul037@gmail.com>
2016-02-02 22:46:57 -0500

    finished temperature reading task, doxygen documentation

--- arduino_chiuhuahua/TemperatureMonitor.c
+++ arduino_chiuhuahua/TemperatureMonitor.c
@@ -1,8 +1,11 @@
-////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////
-////    main.c
-////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////
+/*!	\file TemperatureMonitor.c
+ * \author Patrice Boulet, Ladan Maxamud
+ * \date February 2nd, 2016
+ * \brief Temperature Monitor Module
+ * \details Defines and manage task execution to read temperatures
+ * \from the TemperatureReader module and to display them with
+ * \the LED and LCD modules.
+ */
 
 #include <stdlib.h>
 #include <stdbool.h>
@@ -25,34 +28,26 @@
 
 
 /*-----------------------------------------------------------*/
-/* Create a handle for the serial port. */
-//extern xComPortHandle xSerialPort;
 
-static void TaskGetTemperatures(void *pvParameters);
-
+static void TaskGetTemperatures(uint8_t*);
 /*-----------------------------------------------------------*/
 
 /* Main program loop */
-//int main(void) __attribute__((OS_main));
 int usartfd;
-uint8_t *temperatures;
 
-
+/** Main loop.  Creates tasks, set their order and start the FreeRTOS scheduler.
+ * @return the status of the program when it ends
+ */
 int main(void)
 {
+	uint8_t *temperatures = malloc(sizeof(uint8_t)*10);
 
     // turn on the serial port for debugging or for other USART reasons.
 	usartfd = usartOpen( USART0_ID, 115200, portSERIAL_BUFFER_TX, portSERIAL_BUFFER_RX); //  serial port: WantedBaud, TxQueueLength, RxQueueLength (8n1)
 
-	usart_print_P(PSTR("\r\n\n\nTesting!!!!!\r\n")); // Ok, so we're alive...
+	usart_print_P(PSTR("\r\n\n\nChico: Initializing...\r\n")); // Ok, so we're alive...
 
-	 xTaskCreate(
-	    	TaskGetTemperatures
-			,  (const portCHAR *)"ReadTemperature" // Temperature reading task
-			,  256				// Tested 9 free @ 208
-			,  NULL
-			,  3
-			,  NULL ); // */
+	xTaskCreate(TaskGetTemperatures,  (const portCHAR *)"ReadTemperature" , 256, temperatures, 3,  NULL );
 
 	usart_printf_P(PSTR("\r\n\nFree Heap Size: %u\r\n"),xPortGetFreeHeapSize() ); // needs heap_1 or heap_2 for this function to succeed.
 
@@ -62,9 +57,12 @@ int main(void)
 
 }
 
-static void TaskGetTemperatures(void *pvParameters){
-	(void) pvParameters;;
-	    TickType_t xLastWakeTime;
+/** Gets temperature readings from the TPA81 thermal array sensor
+ *  each 400 ticks.
+ *  @param[temperatures] an array to hold the 9 temperatures to be read
+ */
+static void TaskGetTemperatures(uint8_t *temperatures){
+	    TickType_t xLastWakeTime;							// keeps track of timing
 
 		/* The xLastWakeTime variable needs to be initialised with the current tick
 		count.  Note that this is the only time we access this variable.  From this
@@ -72,23 +70,12 @@ static void TaskGetTemperatures(void *pvParameters){
 		API function. */
 		xLastWakeTime = xTaskGetTickCount();
 
-
 	    while(1)
 	    {
-	    	temperatures = getTemperatureFromSensor();
-	    	usart_printf_P(PSTR("Temperature 1 @ %u\r\n"), temperatures[1]);
-	    	usart_printf_P(PSTR("Temperature 2 @ %u\r\n"), temperatures[2]);
-	    	usart_printf_P(PSTR("Temperature 3 @ %u\r\n"), temperatures[3]);
-	    	usart_printf_P(PSTR("Temperature 4 @ %u\r\n"), temperatures[4]);
-	    	usart_printf_P(PSTR("Temperature 5 @ %u\r\n"), temperatures[5]);
-	    	usart_printf_P(PSTR("Temperature 6 @ %u\r\n"), temperatures[6]);
-	    	usart_printf_P(PSTR("Temperature 7 @ %u\r\n"), temperatures[7]);
-	    	usart_printf_P(PSTR("Temperature 8 @ %u\r\n"), temperatures[8]);
-	    	usart_printf_P(PSTR("Temperature 9 @ %u\r\n"), temperatures[9]);
+	    	getTemperatureFromSensor(temperatures);
 			vTaskDelayUntil( &xLastWakeTime, ( 400 / portTICK_PERIOD_MS ) );
 	    }
 }
-
 
 void vApplicationStackOverflowHook( TaskHandle_t xTask,
                                     portCHAR *pcTaskName )
@@ -98,6 +85,4 @@ void vApplicationStackOverflowHook( TaskHandle_t xTask,
 	PORTB |= _BV(PORTB7);       // main (red PB7) LED on. Mega main LED on and die.
 	while(1);
 }
-
-/*-----------------------------------------------------------*/
 
--- arduino_chiuhuahua/TemperatureReader.c
+++ arduino_chiuhuahua/TemperatureReader.c
@@ -1,18 +1,8 @@
-/*
- * ThermalSensor.c
- *
- */
-
-/*(Doxygen help: use \brief to provide short summary and \details command can be used)*/
-
-/*!	\file template.c
- * \author Ladan Maxamud and Patrice Boulet
+/*!	\file TemperatureReader.c
+ * \author Ladan Maxamud, Patrice Boulet
  * \date January 27th 2016
- * \brief Thermal Sensor reader
+ * \brief Temperature Reader Module
  * \details This module communicates with the thermal sensor TPA81 using the I2C bus protocol.
- *
- *
- *
  */
 
 /* --Includes-- */
@@ -20,23 +10,15 @@
 
 /*---------------------------------------  Function Declarations  -------------------------------------------------*/
 void TemperatureSensor(void);
-uint8_t *getTemperatureFromSensor(void);
+void getTemperatureFromSensor(uint8_t*);
 
-/*! \brief Main entry point for temperature module
- *
- * \details This function
- * \note main() function can be only appear in one module, which is the main entry point of program;
- * hence remove it while coding a supporting module, which itself is not suppose to execute.
- *
- *
- * @return void
- */
+
 void TemperatureSensor(void){
 	I2C_Master_Initialise(0xC0);
 }
 
 /*---------------------------------------  LOCAL FUNCTIONS  ------------------------------------------------------*/
-uint8_t *getTemperatureFromSensor(void){
+void getTemperatureFromSensor(uint8_t *temperatures){
 
 	/* 1. Send a start sequence
 	 * 2. Send 0xD0 ( I2C address of the thermal sensor with the R/W bit low (even address)
@@ -48,12 +30,9 @@ uint8_t *getTemperatureFromSensor(void){
 	 * */
 
 	uint8_t writeCommand[2] = {0xD0, 0x01};
-	uint8_t *readCommand = malloc(sizeof(uint8_t)*10);
-	readCommand[0] = 0xD1;
+	temperatures[0] = 0xD1;
 
 	I2C_Master_Start_Transceiver_With_Data(writeCommand,2);
-	I2C_Master_Start_Transceiver_With_Data(readCommand,10);
-	I2C_Master_Get_Data_From_Transceiver(readCommand,10);
-
-	return readCommand;
+	I2C_Master_Start_Transceiver_With_Data(temperatures,10);
+	I2C_Master_Get_Data_From_Transceiver(temperatures,10);
 }
