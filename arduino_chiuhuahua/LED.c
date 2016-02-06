f9fdae933c35577d6b734caf3f95109edd6cf49c
Adnane <ad.gasmi@gmail.com>
2016-02-06 11:37:15 -0500

    addmissing doxygen comments to LED.c

--- arduino_chiuhuahua/LED.c
+++ arduino_chiuhuahua/LED.c
@@ -1,34 +1,50 @@
-#include "include/LED.h"
+#include "LED.h"
 
+/**
+ * Function: redLED
+ * Returns: None
+ * Desc: Turn on or off the red LED depending on passed parameter  
+ */
 void redLED(int on){
 	if(on == 1){
-		DDRH |= _BV(DDH3);
-		 PORTH &= ~_BV(PORTH3);
+		DDRH |= _BV(DDH3); /**< set proper pin in data register */
+		 PORTH &= ~_BV(PORTH3); /**< turn on LED by setting proper pin to 0 */
 	  }else{
 		  if (on == 0){
-		  PORTH |= _BV(PORTH3);
+		  PORTH |= _BV(PORTH3); /**< set proper pin to 1 to turn off LED */
 		  }
 	  }
 }
 
+/**
+ * Function: greenLED
+ * Returns: None
+ * Desc: Turn on or off the green LED depending on passed parameter  
+ */
 void greenLED(int on) {
   if(on == 1){
-	  DDRE |= _BV(DDE3);
-	  PORTE &= ~_BV(PORTE3);
+	  DDRE |= _BV(DDE3); /**< set proper pin in data register */
+	  PORTE &= ~_BV(PORTE3); /**< turn on LED by setting proper pin to 0 */
    }else{
 	  if(on == 0){
-	  PORTE |= _BV(PORTE3);
+	  PORTE |= _BV(PORTE3); /**< set proper pin to 1 to turn off LED */
 	  }
   }
 }
 
+
+/**
+ * Function: blueLED
+ * Returns: None
+ * Desc: Turn on or off the blue LED depending on passed parameter  
+ */
 void blueLED(int on) {
   if(on == 1){
-	  DDRE |= _BV(DDE5);
-	  PORTE &= ~_BV(PORTE5);
+	  DDRE |= _BV(DDE5); /**< set proper pin in data register */
+	  PORTE &= ~_BV(PORTE5); /**< turn on LED by setting proper pin to 0 */
   }else{
 	  if(on == 0){
-	  PORTE |= _BV(PORTE5);
+	  PORTE |= _BV(PORTE5); /**< set proper pin to 1 to turn off LED */
 	  }
   }
 }
