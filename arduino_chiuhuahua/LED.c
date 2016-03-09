f0555ced9321a8998d62100356d33595cb5791d2
Adnane <ad.gasmi@gmail.com>
2016-03-09 18:26:30 -0500

    enum refactor

--- arduino_chiuhuahua/LED.c
+++ arduino_chiuhuahua/LED.c
@@ -7,25 +7,28 @@
  * Returns: None
  * Desc: Turn on the proper LED depending on passed parameter
  */
-void lightLED(char light){
-    if(light == 'r'){
-        redLED(1);
-    }
-    if(light == 'g'){
-        greenLED(1);
-    }
-    if(light == 'b'){
-        blueLED(1);
-    }
-    if(light == 'w'){
-        redLED(1);
-        greenLED(1);
-        blueLED(1);
-    }
-    if(light == 'o'){
-        redLED(0);
-        greenLED(0);
-        blueLED(0);
+void lightLED(LEDState LED){
+    switch(LED)
+    {
+        case RED:
+            redLED(1);
+            break;
+        case GREEN:
+            greenLED(1);
+            break;
+        case BLUE:
+            blueLED(1);
+            break;
+        case WHITE:
+            redLED(1);
+            greenLED(1);
+            blueLED(1);
+            break;
+        case OFF:
+            redLED(0);
+            greenLED(0);
+            blueLED(0);
+            break;
     }
 }
 
@@ -33,48 +36,48 @@ void lightLED(char light){
 /**
  * Function: redLED
  * Returns: None
- * Desc: Turn on or off the red LED depending on passed parameter  
+ * Desc: Turn on or off the red LED depending on passed parameter
  */
 void redLED(int on){
-	if(on == 1){
-		DDRH |= _BV(DDH3); /**< set proper pin in data register */
-		 PORTH &= ~_BV(PORTH3); /**< turn on LED by setting proper pin to 0 */
-	  }else{
-		  if (on == 0){
-		  PORTH |= _BV(PORTH3); /**< set proper pin to 1 to turn off LED */
-		  }
-	  }
+    if(on == 1){
+        DDRH |= _BV(DDH3); /**< set proper pin in data register */
+        PORTH &= ~_BV(PORTH3); /**< turn on LED by setting proper pin to 0 */
+    }else{
+        if (on == 0){
+            PORTH |= _BV(PORTH3); /**< set proper pin to 1 to turn off LED */
+        }
+    }
 }
 
 /**
  * Function: greenLED
  * Returns: None
- * Desc: Turn on or off the green LED depending on passed parameter  
+ * Desc: Turn on or off the green LED depending on passed parameter
  */
 void greenLED(int on) {
-  if(on == 1){
-	  DDRE |= _BV(DDE3); /**< set proper pin in data register */
-	  PORTE &= ~_BV(PORTE3); /**< turn on LED by setting proper pin to 0 */
-   }else{
-	  if(on == 0){
-	  PORTE |= _BV(PORTE3); /**< set proper pin to 1 to turn off LED */
-	  }
-  }
+    if(on == 1){
+        DDRE |= _BV(DDE3); /**< set proper pin in data register */
+        PORTE &= ~_BV(PORTE3); /**< turn on LED by setting proper pin to 0 */
+    }else{
+        if(on == 0){
+            PORTE |= _BV(PORTE3); /**< set proper pin to 1 to turn off LED */
+        }
+    }
 }
 
 
 /**
  * Function: blueLED
  * Returns: None
- * Desc: Turn on or off the blue LED depending on passed parameter  
+ * Desc: Turn on or off the blue LED depending on passed parameter
  */
 void blueLED(int on) {
-  if(on == 1){
-	  DDRE |= _BV(DDE5); /**< set proper pin in data register */
-	  PORTE &= ~_BV(PORTE5); /**< turn on LED by setting proper pin to 0 */
-  }else{
-	  if(on == 0){
-	  PORTE |= _BV(PORTE5); /**< set proper pin to 1 to turn off LED */
-	  }
-  }
+    if(on == 1){
+        DDRE |= _BV(DDE5); /**< set proper pin in data register */
+        PORTE &= ~_BV(PORTE5); /**< turn on LED by setting proper pin to 0 */
+    }else{
+        if(on == 0){
+            PORTE |= _BV(PORTE5); /**< set proper pin to 1 to turn off LED */
+        }
+    }
 }
--- arduino_chiuhuahua/include/LED.h
+++ arduino_chiuhuahua/include/LED.h
@@ -3,10 +3,13 @@
 
 #include "avr/io.h"
 
+typedef enum {RED, GREEN, BLUE, WHITE, OFF} LEDState;
+
+
 /**Turns on the LED passed as a parameter, or turns the LED off
  * @param[in] LED to turn on, or parameter to turn off LED.
  */
-void lightLED(char light);
+void lightLED(LEDState);
 
 
 
