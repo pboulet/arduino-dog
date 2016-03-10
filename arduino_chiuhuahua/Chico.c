f49c1ec42ef9fb20916544fec8aa6c266bc16c39
patriceboulet <pboul037@gmail.com>
2016-03-10 09:14:46 -0500

    definition documentation for tasks, augmented instrument cluster task with average temperatures

--- arduino_chiuhuahua/Chico.c
+++ arduino_chiuhuahua/Chico.c
@@ -1,5 +1,4 @@
 /*!	\file TemperatureMonitor.c
- * \author Patrice Boulet, Ladan Maxamud
  * \date February 2nd, 2016
  * \brief Temperature Monitor Module
  * \details Defines and manage task execution to read temperatures
@@ -32,27 +31,63 @@
 
 /*-----------------------------------------------------------*/
 
+/**
+ * A cyclic scheduler implementation.
+ */
 static void CyclicScheduler(void*);
+
+/**
+ * Gets temperature readings from the TPA81 thermal array sensor.
+ */
 static void ReadTemperatures(void);
+
+/**
+ * Reads the current speed of both wheels.
+ */
 static void ReadSpeed(void);
+
+/**
+ * Moves the robot in the currently selected direction.
+ */
 static void Move(void);
 
-/**Sets the Hydrogen Wifi shield LED based on the temperature readings
- * @param[in] Array of eight uint8_t, representing the 8-pixel temperature readings
+/**
+ * Sets the Hydrogen Wifi shield LED based on the temperature readings
  */
 static void UpdateLED(void);
-static void DisplayTemperatures(void);
+
+/**
+ * Updates the values shown on the LCD screen according to the most current
+ * readings from the different sensors of the robot.
+ */
 static void UpdateInstrumentCluster(void);
+
+/**
+ * Gets the currently selected motion mode (or direction) of
+ * the robot.
+ */
 static MotionMode GetMotionMode(void);
+
 /*-----------------------------------------------------------*/
 
-/* Main program loop */
 int usartfd;
+
+/* Total distance traveled by the robot. */
 float distanceTraveled;
+
+/* Holds the most current temperature reading from the thermal array sensor. */
 uint8_t *temperatures;
+
+/* Holds the most current speed reading from the encoder of the left wheel. */
 float *speedLeft;
+
+/* Holds the most current speed reading from the encoder of the right wheel. */
 float *speedRight;
+
+/* Holds the most current total distance traveled by the robot. */
 float *distance;
+
+/* Holds the most current position of the thermal array servo motor. */
 uint16_t *centerServoPosition;
 
 
@@ -61,15 +96,13 @@ typedef void (*TASK)(void);
 #define NUM_MINOR_CYCLES 132
 #define MINOR_CYCLE_TIME 50
 
+/* Cyclic scheduler task table. */
 TASK table[NUM_MINOR_CYCLES] = {
-		UpdateLED,
 		Move,
 		ReadTemperatures,
 		ReadSpeed,
 		UpdateInstrumentCluster,
-		UpdateLED,
-		ReadSpeed,
-		UpdateInstrumentCluster
+		UpdateLED
 };
 
 /** Main loop.  Creates tasks, set their order and start the FreeRTOS scheduler.
@@ -128,9 +161,6 @@ static void ReadSpeed() {
 	readSpeed(speedLeft, speedRight, distance);
 }
 
-/** Gets temperature readings from the TPA81 thermal array sensor.
- *  @param[temperatures] an array to hold the 9 temperatures to be read
- */
 static void ReadTemperatures(void) {
 	temperatureSweep(temperatures, centerServoPosition);
 	getTemperatureFromSensor(temperatures);
@@ -158,55 +188,37 @@ static void UpdateLED()
 static void UpdateInstrumentCluster(void){
 		clearLCD();
 
-		/* 	Prints the current speed and distanced traveled in the current direction
-			on the first line of the LCD screen. */
+		/* Average speed of the two wheels. */
 		float speed = (*speedLeft + *speedRight)/ 2.0F;
 
+		/* Average of the first four temperature pixels from the left. */
+		float tempAvgLeft = (temperatures[0] + temperatures[1] + temperatures[2] + temperatures[3])/4.0F;
+
+		/* Average of the first four temperature pixels from the right. */
+		float tempAvgRight = (temperatures[4] + temperatures[5] + temperatures[6] + temperatures[7])/4.0F;
+
 		char topRow[16];
+		char bottomRow[16];
+
+		/*
+		 * Prints the average speed of the two wheels as well as the
+		 * total distance traveled on the first line of the LCD screen.
+		 */
 		sprintf(topRow,
 			"%.2f %.2f",
 			speed,
 			*distance);
-
 		writeLCDRowOne(topRow);
-/*
-		char bottomRow[16];
 
+		/*
+		 * Prints the average temperature for the first four pixel sensors from the left
+		 * as well as the average temperature for the first four pixel sensors from the right.
+		 */
 		sprintf(bottomRow,
-			"%02d %02d %02d %02d",
-			temperatures[5],
-			temperatures[6],
-			temperatures[7],
-			temperatures[8]);
-
-		writeLCDRowTwo(bottomRow);*/
-}
-
-// deprecated
-static void DisplayTemperatures() {
-	clearLCD();
-
-	char topRow[16];
-	sprintf(topRow,
-		"%02d %02d %02d %02d [%02d]",
-		temperatures[1],
-		temperatures[2],
-		temperatures[3],
-		temperatures[4],
-		temperatures[0]);
-
-	writeLCDRowOne(topRow);
-
-	char bottomRow[16];
-
-	sprintf(bottomRow,
-		"%02d %02d %02d %02d",
-		temperatures[5],
-		temperatures[6],
-		temperatures[7],
-		temperatures[8]);
-
-	writeLCDRowTwo(bottomRow);
+			"%.2f %.2f",
+			tempAvgLeft,
+			tempAvgRight);
+		writeLCDRowTwo(bottomRow);
 }
 
 static void Move(void){
